0ac0bd799e69c18504dbdfaf17fef19fab7253a0
Baiju Dodhia <51258945+baijudodhia@users.noreply.github.com>
2020-09-23 01:19:16 +0530

    Content + Design + Dark Mode

---
+++ main.js
@@ -0,0 +1,17 @@
+function changeTheme() {
+    var theme = document.getElementById('theme-nav-brand').getAttribute('theme-value');
+    var theme = document.getElementById('theme-nav-item').getAttribute('theme-value');
+    if (theme === "dark") {
+        document.getElementById('theme-nav-brand').setAttribute("theme-value", "light");
+        document.getElementById('theme-nav-item').setAttribute("theme-value", "light");
+        document.getElementById('theme-nav-brand').value = "🌑";
+        document.getElementById('theme-nav-item').value = "🌑";
+        document.getElementsByTagName("html")[0].setAttribute("data-theme", "light");
+    } else {
+        document.getElementById('theme-nav-brand').setAttribute("theme-value", "dark");
+        document.getElementById('theme-nav-item').setAttribute("theme-value", "dark");
+        document.getElementById('theme-nav-brand').value = "🌕";
+        document.getElementById('theme-nav-item').value = "🌕";
+        document.getElementsByTagName("html")[0].setAttribute("data-theme", "dark");
+    }
+};
