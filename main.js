a2a9217cee01ca8d82e4d6ad07fda1d3a145f9f5
Baiju Dodhia <51258945+baijudodhia@users.noreply.github.com>
2021-05-03 05:24:42 +0530

    Removed Bootstrap/Font Awesome CDN & added necessary CSS to main.css + add & updated content + UI Design changes

--- main.js
+++ main.js
@@ -1,20 +1,15 @@
 function changeTheme() {
-    var theme = document.getElementById('theme-nav-brand').getAttribute('theme-value');
-    var theme = document.getElementById('theme-nav-item').getAttribute('theme-value');
+    var theme = document.getElementById("theme-nav-brand").getAttribute("theme-value");
     if (theme === "dark") {
-        document.getElementById('theme-nav-brand').setAttribute("theme-value", "light");
-        document.getElementById('theme-nav-item').setAttribute("theme-value", "light");
-        document.getElementById('theme-nav-brand').value = "🌑";
-        document.getElementById('theme-nav-item').value = "🌑";
+        document.getElementById("theme-nav-brand").setAttribute("theme-value", "light");
+        document.getElementById("theme-nav-brand").value = "🌑";
         document.getElementsByTagName("html")[0].setAttribute("data-theme", "light");
     } else {
-        document.getElementById('theme-nav-brand').setAttribute("theme-value", "dark");
-        document.getElementById('theme-nav-item').setAttribute("theme-value", "dark");
-        document.getElementById('theme-nav-brand').value = "🌕";
-        document.getElementById('theme-nav-item').value = "🌕";
+        document.getElementById("theme-nav-brand").setAttribute("theme-value", "dark");
+        document.getElementById("theme-nav-brand").value = "🌕";
         document.getElementsByTagName("html")[0].setAttribute("data-theme", "dark");
     }
-};
+}
 
 function goTop() {
     document.body.scrollTop = 0;
