76ed360c039d4706bc16af192f1a880ec906e6c3
baijudodhia <51258945+baijudodhia@users.noreply.github.com>
2022-01-31 17:16:01 +0530

    Update: Theme Changer UI changed from button to toggle button style.

--- components/base/theme-changer.js
+++ components/base/theme-changer.js
@@ -1,15 +1,79 @@
 const ThemeChangerTemplate = document.createElement('template');
 ThemeChangerTemplate.innerHTML = `
     <link href="../../main.css" rel="stylesheet">
-    <style></style>
-    <input
-        type="submit"
-        name="theme"
-        id="theme-changer"
-        value="🌕"
-        theme="dark"
-        onclick="changeTheme();"
-    />
+    <style>
+        #theme-changer-container {
+            display: flex;
+            flex-direction: row;
+            align-content: center;
+            justify-content: center;
+            align-items: center;
+            padding: 0px 12.5px;
+        }
+        .theme-changer-switch {
+            position: relative;
+            display: inline-block;
+            width: 25px;
+            height: 15px;
+        }
+        #theme-changer {
+            height: 0;
+            opacity: 0;
+            overflow: visible;
+            padding: 0;
+            margin: 0;
+            width: 0;
+        }
+        .theme-changer-slider {
+            position: absolute;
+            top: 0;
+            left: 0;
+            right: 0;
+            bottom: 0;
+            cursor: pointer;
+            background-color: #ccc;
+            border-radius: 20px;
+            transition: 0s;
+        }
+        .theme-changer-slider:before {
+            content: "";
+            position: absolute;
+            left: 0%;
+            bottom: -5px;
+            height: 1.5rem;
+            width: 1.5rem;
+            transform: translateX(-50%) rotate(-30deg);
+            transition: 0.25s;
+            background-position: center !important;
+            background-repeat: no-repeat !important;
+            background-size: 70% !important;
+            background: #fff url("/assets/img/theme-changer/light.png");
+            border-radius: 50%;
+            box-shadow: 0px 0px 5px -2px #121212;
+        }
+        #theme-changer:checked + .theme-changer-slider {
+            background-color: #009aff;
+            transition: 0.5s;
+        }
+        #theme-changer:checked + .theme-changer-slider:before {
+            left: 100%;
+            background-size: 65% !important;
+            transform: translateX(-50%) rotate(0deg);
+            background: #fff url("/assets/img/theme-changer/dark.png");
+            box-shadow: 0px 0px 10px -3px #f5f5f5;
+        }
+    </style>
+    <div id="theme-changer-container">
+        <label class="theme-changer-switch">
+            <input 
+                id="theme-changer"
+                name="theme"
+                onchange="changeTheme();" 
+                type="checkbox"
+            />
+            <span class="theme-changer-slider"></span>
+        </label>
+    </div>
 `;
 
 class AppThemeChanger extends HTMLElement {
@@ -40,10 +104,12 @@ class AppThemeChanger extends HTMLElement {
 
     attributeChangedCallback(name, oldValue, newValue) {
         // called when one of attributes listed above is modified
-        if(name === "value" && oldValue !== null) {
+        if (name === "value" && oldValue !== null) {
             this.shadowRoot.querySelector("#theme-changer").setAttribute("value", newValue);
         }
-        if(name === "theme" && oldValue !== null) {
+        if (name === "theme" && oldValue !== null) {
+            let themeChangerChecked = newValue === 'dark' ? true : false;
+            this.shadowRoot.querySelector("#theme-changer").checked = themeChangerChecked;
             this.shadowRoot.querySelector("#theme-changer").setAttribute("theme", newValue);
         }
     }
