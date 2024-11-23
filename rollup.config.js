778a3e41e49115ac330ea00d254233e5c6be41ed
Baiju Dodhia <51258945+baijudodhia@users.noreply.github.com>
2024-11-23 19:49:04 +0530

    Update: bundler config optimized for creating only a single small size bundle instead of multiple files
    Signed-off-by: Baiju Dodhia <51258945+baijudodhia@users.noreply.github.com>

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,5 @@
 import commonjs from "@rollup/plugin-commonjs";
 import resolve from "@rollup/plugin-node-resolve";
-import fs from "fs";
-import path from "path";
 import postcssImport from "postcss-import";
 import postcssUrl from "postcss-url";
 import html from "rollup-plugin-html";
@@ -9,140 +7,87 @@ import postcss from "rollup-plugin-postcss";
 import { terser } from "rollup-plugin-terser";
 import url from "rollup-plugin-url";
 
-// Helper function to convert component names to valid JS identifiers
-function toValidIdentifier(name) {
-  return name.replace(/-([a-z])/g, (_, letter) => letter.toUpperCase()).replace(/[^a-zA-Z0-9_$]/g, "");
-}
-
-// Recursive function to get component configurations
-function getStyleConfigs(dir) {
-  const configs = [];
-  const items = fs.readdirSync(dir, { withFileTypes: true });
-
-  items.forEach((item) => {
-    const fullPath = path.join(dir, item.name);
-
-    if (item.isDirectory()) {
-      configs.push(...getComponentConfigs(fullPath));
-    } else if (item.isFile() && item.name.endsWith(".css")) {
-      const componentName = path.basename(item.name, ".css");
-      const componentDir = path.dirname(fullPath).replace(/\\/g, "/");
-
-      const outputFilePath = `lib/dist/${componentDir.replace("^(lib/)$", "")}/${componentName}.min.css`.replace("/lib/", "/");
-      const outputFileName = toValidIdentifier(componentName.charAt(0).toUpperCase() + componentName.slice(1));
-
-      configs.push({
-        input: fullPath,
-        output: {
-          file: outputFilePath,
-          assetFileNames: "[name].min.[extname]",
-          sourcemap: false,
+const plugins = [
+  // First postcss instance for component styles - injected into JS
+  postcss({
+    inject: true, // Inject into JS
+    extract: false, // Don't extract component styles
+    minimize: true,
+    modules: false,
+    sourceMap: false,
+    include: ["**/components/**/*.css"], // Only process component styles
+    plugins: [
+      postcssImport(),
+      postcssUrl({
+        url: (asset) => {
+          if (asset.url.startsWith("http")) {
+            return asset.url;
+          }
+          return asset.url;
         },
-        plugins: [
-          postcss({
-            extract: false,
-            minimize: true,
-            sourceMap: false,
-            plugins: [
-              postcssImport(),
-              postcssUrl({
-                url: (asset) => {
-                  if (asset.url.startsWith("http")) {
-                    return asset.url;
-                  }
-                  return asset.url;
-                },
-                inline: true,
-              }),
-            ],
-            config: {
-              path: "./postcss.config.js",
-            },
-          }),
-          url({
-            include: ["**/*.svg", "**/*.png", "**/*.jpg", "**/*.gif", "**/*.woff", "**/*.woff2"],
-            limit: 8192,
-            emitFiles: true,
-          }),
-          resolve(),
-          commonjs(),
-          html({
-            include: "**/*.html",
-          }),
-          terser(),
-        ],
-      });
-    }
-  });
-
-  return configs;
-}
-
-// Recursive function to get component configurations
-function getComponentConfigs(dir) {
-  const configs = [];
-  const items = fs.readdirSync(dir, { withFileTypes: true });
-
-  items.forEach((item) => {
-    const fullPath = path.join(dir, item.name);
-
-    if (item.isDirectory()) {
-      configs.push(...getComponentConfigs(fullPath));
-    } else if (item.isFile() && item.name.endsWith(".js")) {
-      const componentName = path.basename(item.name, ".js");
-      const componentDir = path.dirname(fullPath).replace(/\\/g, "/");
-
-      const outputFilePath = `lib/dist/${componentDir.replace("^(lib/)$", "")}/${componentName}.min.js`.replace("/lib/", "/");
-      const outputFileName = toValidIdentifier(componentName.charAt(0).toUpperCase() + componentName.slice(1));
-
-      configs.push({
-        input: fullPath,
-        output: {
-          file: outputFilePath,
-          format: "iife",
-          name: outputFileName,
-          sourcemap: false,
+        inline: true,
+      }),
+    ],
+  }),
+  // Second postcss instance for global styles - extracted to CSS file
+  postcss({
+    inject: false, // Don't inject global styles
+    extract: "index.min.css", // Extract to CSS file
+    minimize: true,
+    modules: false,
+    sourceMap: false,
+    include: ["**/styles/**/*.css"], // Only process global styles
+    plugins: [
+      postcssImport(),
+      postcssUrl({
+        url: (asset) => {
+          if (asset.url.startsWith("http")) {
+            return asset.url;
+          }
+          return asset.url;
         },
-        plugins: [
-          postcss({
-            extract: false,
-            minimize: true,
-            sourceMap: false,
-            use: ["sass"],
-            config: {
-              path: "./postcss.config.js",
-            },
-            plugins: [
-              postcssImport(),
-              postcssUrl({
-                url: (asset) => {
-                  if (asset.url.startsWith("http")) {
-                    return asset.url;
-                  }
-                  return asset.url;
-                },
-                inline: true,
-              }),
-            ],
-          }),
-          url({
-            include: ["**/*.svg", "**/*.png", "**/*.jpg", "**/*.gif", "**/*.woff", "**/*.woff2"],
-            limit: 8192,
-            emitFiles: true,
-          }),
-          resolve(),
-          commonjs(),
-          html({
-            include: "**/*.html",
-          }),
-          terser(),
-        ],
-      });
-    }
-  });
+        inline: true,
+      }),
+    ],
+  }),
+  url({
+    include: ["**/*.svg", "**/*.png", "**/*.jpg", "**/*.gif", "**/*.woff", "**/*.woff2"],
+    limit: 8192,
+    emitFiles: true,
+  }),
+  resolve({
+    browser: true,
+    preferBuiltins: false,
+  }),
+  commonjs({
+    transformMixedEsModules: true,
+  }),
+  html({
+    include: "**/*.html",
+    minify: true,
+  }),
+  terser({
+    format: {
+      comments: false,
+    },
+    compress: {
+      drop_console: true,
+      drop_debugger: true,
+    },
+  }),
+];
 
-  return configs;
-}
-
-// Export all configurations
-export default [...getStyleConfigs("./lib/styles/"), ...getComponentConfigs("./lib/")];
+export default {
+  input: "./lib/index.js",
+  output: {
+    file: "lib/dist/index.min.js",
+    format: "iife",
+    name: "Components",
+    sourcemap: false,
+  },
+  plugins,
+  treeshake: {
+    moduleSideEffects: true,
+    propertyReadSideEffects: false,
+  },
+};
